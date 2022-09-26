3d44bbe9c5434f2f28ed907467d0a8893e48af76
Firda yulianti <Firda yulianti@LAPTOP-KIF3SKT8>
2022-09-26 16:24:51 +0700

    Add project files.

---
+++ P2_1_1214058/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1214058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("angka pertama = ");
+            int angka1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("angka kedua = ");
+            int angka2 = int.Parse(Console.ReadLine());
+
+            int penjumlahan, pengurangan, pembagian, perkalian;
+            penjumlahan = angka1 + angka2;
+            pengurangan = angka1 - angka2;
+            pembagian   = angka1 / angka2;
+            perkalian   = angka1 * angka2;
+
+            Console.WriteLine("{0} + {1} = {2}", angka1, angka2, penjumlahan);
+            Console.WriteLine("{0} - {1} = {2}", angka1, angka2, pengurangan);
+            Console.WriteLine("{0} / {1} = {2}", angka1, angka2, pembagian);
+            Console.WriteLine("{0} * {1} = {2}", angka1, angka2, perkalian);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
